83401f6d388a1328abd94a347828e29402e9842e
victor <vmataba0@gmail.com>
2023-09-26 22:16:30 +0300

    initialize project

---
+++ src/main/kotlin/com/tabaapps/cashrequisitionretirement/App.kt
@@ -0,0 +1,11 @@
+package com.tabaapps.cashrequisitionretirement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CashRequisitionRetirementApplication
+
+fun main(args: Array<String>) {
+	runApplication<CashRequisitionRetirementApplication>(*args)
+}
